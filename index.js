4bd5a1f82534e1a05b77a6adf59b9d38d0493b97
Nikithassree09 <nikithassree09@gmail.com>
2024-04-18 10:58:02 +0530

    started the server -- added express dependency

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const mongoose =require('mongoose');
 const config =require('./utils/config');
-console.log('Connecting to MongoDB:', config.MONGODB_URI);
+const app = require('./app');
+console.log('Connecting to MongoDB');
 mongoose.connect(config.MONGODB_URI)
   .then(()=> {
     console.log('Connected to MongoDB');
-  })
+ 
+    app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+   });
+}) 
   .catch((error)=> {
     console.log('Error connecting to MongoDB:', error.message);
    })
