60548ee53b5cfc6bf8ee4682e7af1d3bd9d11973
Nikithassree09 <nikithassree09@gmail.com>
2024-04-24 19:33:40 +0530

    login endpoint revised

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const app = express();
 app.use(cors({
     origin: '*',
     credentials: true
 }));
-
+app.use(morgan('dev'));
 app.use(cookieParser());
 //enable express application to parse json
 app.use(express.json());
